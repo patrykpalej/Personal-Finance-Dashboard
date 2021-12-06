93328f1b54ea9f431cec6f02b55ed0a5657977fc
Patryk Palej <patrykpalej@gmail.com>
2021-12-06 19:59:47 +0000

    Add authentication

--- src/app.py
+++ src/app.py
@@ -1,5 +1,10 @@
 import dash
+import dash_auth
+
 from layout import layout
+from config import dash_auth_password
+
 
 app = dash.Dash(__name__, suppress_callback_exceptions=True)
 app.layout = layout()
+auth = dash_auth.BasicAuth(app, {"patryk": dash_auth_password})
