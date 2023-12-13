faec9d7bd8d4e720dda07317a8895331a9c00f79
Patryk Palej <patrykpalej@gmail.com>
2023-12-13 22:07:27 +0100

    Modify filters in search

--- src/backend/tab_4_search_spendings.py
+++ src/backend/tab_4_search_spendings.py
@@ -14,14 +14,15 @@ def make_a_search(spendings, search_phrase):
     """
     if search_phrase is None:
         search_phrase = ""
-    # return spendings[spendings["description"].apply(lambda x: search_phrase.lower() in x.lower())]
+
     return spendings[spendings["description"].apply(lambda x: bool(re.search(search_phrase.lower(), x.lower())))]
 
 
 def search_spendings(categories, search_phrase, start_date, end_date):
-    categories_filter = f''' category in ({','.join(categories)})'''
-    if "'all'" in categories:
-        categories_filter += " OR 1=1"
+    if not "'all'" in categories:
+        categories_filter = f''' category in ({','.join(categories)})'''
+    else:
+        categories_filter = None
 
     spendings = get_data("home_spendings", start_date, end_date, where=categories_filter)
     searched_results = make_a_search(spendings, search_phrase)
